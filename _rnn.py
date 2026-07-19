4f6bd857be3439c1e1f0eb35c115e337495a6515
Kuchana Keerthana <kuchanakeerthana123@gmail.com>
2026-07-19 11:28:07 +0530

    Delete _rnn.py

--- _rnn.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-
-model = Sequential()
-
-model.add(LSTM(32, input_shape=(5,1)))
-model.add(Dense(1))
-
-model.summary()
